765e438e33523e1b9a3d11c6852b45d260353104
r3ndo0 <r3ndo0@gmail.com>
2023-07-10 12:11:18 +0330

    moving on to second step , snackbar left out from step 1

---
+++ components/Step2/Inputs/Input.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="relative flex gap-2 flex-col justify-center  p-6 items-start">
+        <IconsAsterisk class="absolute top-4 right-5"/>
+        <label class="px-3">عنوان تقاضا</label>
+        <input class="p-2 rounded-lg"/>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+interface Props {
+    label:string;
+    
+
+}
+
+
+
+</script>
